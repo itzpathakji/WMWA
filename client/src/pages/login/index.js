929edfb80984e2db6c0a31d951e0a811a3fd8ef5
Shubham Kumar Pathak <shubhampathak26092002@gmail.com>
2023-10-19 17:08:32 +0530

    Login Page Changes

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import { Form, Input, Button, Divider } from 'antd';
+import { Link } from 'react-router-dom';
 
 function Login() {
+
+  const onFinish = (values) => {
+    console.log("success:",values);
+  }
+
   return (
-    <div>Login</div>
+    <div className='grid grid-cols-2'>
+      <div className='bg-primary h-screen'></div>
+      <div className='flex justify-center items-center'>
+        <div className='w-[425px]'>
+          <h1 className='text-2xl text-gray-700'>LOGIN INTO YOUR ACCOUNT</h1>
+          <Divider />
+          <Form layout="vertical" onFinish={onFinish}>
+            <Form.Item label="Email" name="email">
+              <Input />
+            </Form.Item>
+            <Form.Item label="Password" name="password">
+              <Input type="password" />
+            </Form.Item>
+
+            <Button type='primary' htmlType='submit' block>Login</Button>
+
+            <div className='flex justify-center mt-5'>
+              <span>
+                Don't have an account? <Link to='/register'>Register</Link>
+              </span>
+            </div>
+          </Form>
+        </div>
+      </div>
+    </div>
   )
 }
 
