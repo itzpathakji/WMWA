78ef9ff0acf5764dc1a52915cb7bfe9b19a75271
Priyansh Lohiya <priyanshlohiya2002@gmail.com>
2024-02-08 19:53:53 +0530

    redux componects user slice is added

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,16 +1,3 @@
-// import moment from "moment";
-
-// export const getAntdFormInputRules = 
-//   [{
-//     required: true,
-//     message: "Required",
-//   },
-// ];
-
-// export const getDateFormat = (date) => {
-//   return moment(date).format("DD-MM-YYYY, hh:mm A");
-// }
-
 import moment from "moment";
 
 export const getAntdFormInputRules = [
