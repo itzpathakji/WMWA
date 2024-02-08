59457682526b9fafd47296e6fd2045466f564b92
Priyansh Lohiya <priyanshlohiya2002@gmail.com>
2024-02-08 19:23:02 +0530

    notification added

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,48 @@
-import {BrowserRouter , Route , Routes} from "react-router-dom";
-
+import { useSelector } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedPage from "./components/ProtectedPage";
+import Spinner from "./components/Spinner";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
-import ProtectedPage from "./components/ProtectedPage";
 import Profile from "./pages/Profile";
-import { useSelector } from "react-redux";
-import Spinner from "./components/Spinner";
 import ProjectInfo from "./pages/ProjectInfo";
+import Register from "./pages/Register";
 
 function App() {
-  const {loading} = useSelector((state) => state.loaders);
+  const { loading } = useSelector((state) => state.loaders);
   return (
-    <div >
+    <div>
       {loading && <Spinner />}
-     <BrowserRouter>
-     <Routes>
-       <Route path ="/" element = {(<ProtectedPage> <Home/> </ProtectedPage>)} />
-       <Route path ="/Project/:id" element = {(<ProtectedPage> <ProjectInfo/> </ProtectedPage>)} />
-       <Route path ="/Profile" element = {(<ProtectedPage> <Profile/> </ProtectedPage>)} />
-       <Route path ="/Login" element = {<Login />} />
-       <Route path ="/Register" element = {<Register />} />
-     </Routes>
-     </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedPage>
+                <Home />
+              </ProtectedPage>
+            }
+          />
+          <Route
+            path="/project/:id"
+            element={
+              <ProtectedPage>
+                <ProjectInfo />
+              </ProtectedPage>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedPage>
+                <Profile />
+              </ProtectedPage>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
